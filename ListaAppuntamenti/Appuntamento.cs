5b3814c885c27adbf64c1dfecf18a2924627db8b
GiorgioAscione <acergio95@libero.it>
2022-04-28 22:08:24 +0200

    Aggiungere i file di progetto.

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    class Appuntamento
+    {
+
+        private string nomeAppuntamento;
+        private string localitaAppuntamento;
+        private DateTime dataOrarioAppuntamento;
+
+        public Appuntamento(string nomeAppuntamento, string localitaAppuntamento, DateTime daraOrarioAppuntamento)
+        {
+            this.nomeAppuntamento = nomeAppuntamento;
+            this.localitaAppuntamento = localitaAppuntamento;
+
+            if (daraOrarioAppuntamento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data inserita non è corretta essendo già passata");
+            }
+            else
+            {
+                this.dataOrarioAppuntamento = daraOrarioAppuntamento;
+            }
+        }
+
+
+        public void InserisciUnaNuovaData(DateTime nuovaData)
+        {
+            if (nuovaData < DateTime.Now)
+            {
+                throw new InvalidDataException("La data inserita non è corretta essendo già passata");
+            }
+            else
+            {
+                this.dataOrarioAppuntamento = nuovaData;
+            }
+        }
+
+        //GETTERS E SETTERS
+
+        public string GetnomeAppuntamento()
+        {
+            return nomeAppuntamento;
+        }
+
+        public string GetLocalitaAppuntamento()
+        {
+            return localitaAppuntamento;
+        }
+
+        public void SetnomeAppuntamento(string nuovoNome)
+        {
+            nomeAppuntamento = nuovoNome;
+        }
+
+        public void SetlocalitaAppuntamento(string nuovaLocalita)
+        {
+            localitaAppuntamento = nuovaLocalita;
+        }
+
+        public override string ToString()
+        {
+            string datiAppuntamento = "------------ \n Appuntamento: " + nomeAppuntamento + "\n Presso: " + localitaAppuntamento + "\n In data: " + dataOrarioAppuntamento;
+            return datiAppuntamento;
+        }
+    }
+}
---
+++ ListaAppuntamenti/Program.cs
@@ -0,0 +1,94 @@
+﻿using ListaAppuntamenti;
+
+Console.WriteLine("------AGENDA APPUNTAMENTI-----");
+
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("Benvenuto");
+Console.WriteLine("Quanti appuntamenti vuoi prenotare?");
+
+int numeroDiAppuntamenti = ChiediNumeroDiAppuntamenti();
+{
+    throw new NotImplementedException();
+}
+
+for (int i = 0; i < numeroDiAppuntamenti; i++)
+{
+    Console.WriteLine("Appuntamento numero " + (i + 1));
+    Console.WriteLine("Aggiungi il nome del tuo appuntamento: ");
+    string nomeAppuntamento = Console.ReadLine();
+    Console.WriteLine("Aggiungi il luogo del tuo appuntamento: ");
+    string localitaAppuntamento = Console.ReadLine();
+
+    bool dataCorretta = false;
+    while (dataCorretta)
+    {
+        DateTime dataOraAppuntamento = InserisciUnaData();
+        try
+        {
+            listaAppuntamenti.Add(new Appuntamento(nomeAppuntamento, localitaAppuntamento, dataOraAppuntamento));
+            dataCorretta = true;
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+}
+
+
+//FUNZIONI 
+
+int ChiediNumeroDiAppuntamenti()
+{
+    int numeroDiAppuntamenti = 0;
+    bool numeroDiAppuntamentiCorretto = false;
+    while (!numeroDiAppuntamentiCorretto)
+    {
+        if (int.TryParse(Console.ReadLine(), out int result))
+        {
+            numeroDiAppuntamenti = result;
+            numeroDiAppuntamentiCorretto = true;
+        }
+        else
+        {
+            Console.WriteLine("Inserisci un numero intero");
+        }
+    }
+    return numeroDiAppuntamenti;
+}
+
+DateTime InserisciUnaData()
+{
+    bool formatoDataCorretto = false;
+    DateTime dataOra = DateTime.Now;
+    while (!formatoDataCorretto)
+    {
+        Console.WriteLine("Aggiungi la data e l' ora del tuo appuntamento: [gg/mm/aa]");
+        try
+        {
+            dataOra = DateTime.Parse(Console.ReadLine());
+            formatoDataCorretto=true;
+        }catch (Exception)
+        {
+            Console.WriteLine("Il formato della data non è corretto");
+        }
+    }
+    return dataOra;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
